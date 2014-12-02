eabd2d3d842ab60f2105e89d903f738b80d95083
Karl Zylinski <karl@zylinski.se>
2014-12-03 00:19:19 +0100

    Most old stuff uses new component system now.

--- game.lua
+++ game.lua
@@ -10,15 +10,19 @@ function Game:init()
     self._view_pos = Vector2(0, 0)
     self._view_size = Vector2(1280, 720)
     self._background_material = Engine.load_resource("material", "background.material")
-    self._background = Rectangle.spawn(self._world, Vector2(0,0), self._view_size, Color(1, 1, 1, 1))
-    Drawable.set_material(self._background, self._background_material)
-    Drawable.set_depth(self._background, -1)
+    self._background = Entity.create(self._world)
+    SpriteRenderer.create(self._background, Vector2(0,0), self._view_size)
+    SpriteRenderer.set_material(self._background, self._background_material)
+    --Drawable.set_depth(self._background, -1)
     
     self._rocket = Rocket()
     self._rocket:spawn(self._world)
     Engine.load_resource("sprite", "sprites/ground.sprite")
-    self._sprite = Sprite.spawn(self._world, "sprites/ground.sprite", 25)
-    self._ground = Drawable.set_position(self._sprite, Vector2(-443,600))
+    self._background_material = Engine.load_resource("material", "sprites/ground.material")
+    self._ground = Entity.create(self._world)
+    SpriteRenderer.create(self._ground, Vector2(0,0), Vector2(1280,400))
+    SpriteRenderer.set_material(self._ground, self._background_material)
+    Transform.set_position(self._ground, Vector2(-443,600))
 end
 
 function Game:deinit()
@@ -34,9 +38,9 @@ function Game:update(dt)
 
     self._rocket:update(dt, self._view_size)
     local rocket_pos = self._rocket:position()
-    --Drawable.set_position(self._background, rocket_pos - self._view_size * 0.5)
+    Transform.set_position(self._background, rocket_pos - self._view_size * 0.5)
     self._view_pos = rocket_pos * -1 + self._view_size * 0.5
-    --Material.set_uniform_value(self._background_material, "height", Vector4(1+(rocket_pos.y/10000), 0, 0, 0))
+    Material.set_uniform_value(self._background_material, "height", Vector4(1+(rocket_pos.y/10000), 0, 0, 0))
     World.update(self._world)
 end
 
